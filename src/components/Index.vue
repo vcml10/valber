6ad0385c5268bd5afb1c30a51f3a8a8550e84e6a
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2018-06-20 21:48:33 -0300

    first commit

---
+++ src/components/Index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="index">
+  <section class="hero is-dark is-fullheight">
+    <div class="hero-head">
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+              <span>Nome do Projeto</span>
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenu">
+              <span></span>
+              <span></span>
+              <span></span>
+              </span>
+            </div>
+            <div id="navbarMenu" class="navbar-menu">
+              <div class="navbar-end">
+                <span class="navbar-item">
+                  <a class="button is-white is-outlined" href="#">
+                    <span>Página Principal</span>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div> <!-- /hero-head -->
+
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <div class="column is-6 is-offset-3">
+            <h1 class="title">
+              Em breve
+            </h1>
+            <h2 class="subtitle">
+              Descrição do projeto...
+            </h2>
+
+          </div>
+        </div>
+      </div> <!-- /hero-body -->
+  </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// Bulma and Buefy - Theme
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+Vue.use(Buefy)
+
+// Local styles and colors
+require('./assets/sass/app.scss')
+require('./assets/css/index.css')
+require('./assets/css/login.css')
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
