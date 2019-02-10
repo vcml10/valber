c6002cb23aefa5051bee36e9bf0f0ec6246e2d55
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2019-02-10 18:55:51 -0300

    mounting dashboard

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,59 +1,15 @@
 <template>
   <div id="index">
     <div class="container">
-  <div class="tile is-ancestor">
-  <div class="tile is-parent">
-    <article class="tile is-child box">
-      <p class="title">Hello World</p>
-      <p class="subtitle">What is up?</p>
-    </article>
-  </div>
-  <div class="tile is-parent">
-    <article class="tile is-child box">
-      <p class="title">Foo</p>
-      <p class="subtitle">Bar</p>
-    </article>
-  </div>
-  <div class="tile is-parent">
-    <article class="tile is-child box">
-      <p class="title">Third column</p>
-      <p class="subtitle">With some content</p>
-      <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-      </div>
-    </article>
-  </div>
-</div>
-
-<div class="tile is-ancestor">
-  <div class="tile is-vertical is-8">
-    <div class="tile">
-      <div class="tile is-parent is-vertical">
-        <article class="tile is-child box">
-          <p class="title">Vertical tiles</p>
-          <p class="subtitle">Top box</p>
-        </article>
-        <article class="tile is-child box">
-          <p class="title">Vertical tiles</p>
-          <p class="subtitle">Bottom box</p>
-        </article>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="tile is-ancestor">
-  <div class="tile is-parent">
-    <article class="tile is-child box">
-      <p class="title">Side column</p>
-      <p class="subtitle">With some content</p>
-      <div class="content">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-      </div>
-    </article>
-  </div>
-</div>
-  </div> <!-- /container -->
+      <div class="tile is-ancestor">
+        <stations-by-city />
+      </div> <!-- /is-ancestor -->
+      <div class="tile is-ancestor">
+        <tile code=1 />
+        <tile code=2 />
+        <tile code=3 />
+      </div> <!-- /is-ancestor -->
+    </div> <!-- /container -->
   </div>
 </template>
 
@@ -63,6 +19,10 @@ export default {
   data () {
     return {
     }
+  },
+  created () {
+  },
+  computed: {
   }
 }
 </script>
